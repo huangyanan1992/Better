bbf7cc6a59490a8ab5fb8e799a9cd0de08893d2f
SQHyn1992 <huangyanan@01zhuanche.com>
2016-10-14 14:20:26 +0800

    Initial Commit

---
+++ Better/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Better
+//
+//  Created by Huang Yanan on 2016/10/14.
+//  Copyright © 2016年 Huang Yanan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
